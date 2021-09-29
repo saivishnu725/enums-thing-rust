1d07dce31a4ad1c3683a4270d23784fe4d4754d3
Sai Vishnu <saivishnu725@gmail.com>
2021-09-29 19:19:07 +0530

    enums and values inside it.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+#[derive(Debug)]
+enum IpAddr {
+    v4(String),
+    v6(String),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    Color(i32, i32, i32),
+}
+
+impl Message {
+    // impl works for both enum and struct and is used to create a function related to the struct/enum type.
+    fn func() {
+        println!("Hello kids!!");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let four = IpAddr::v4(String::from("localhost"));
+    println!("{:#?}", four);
 }
